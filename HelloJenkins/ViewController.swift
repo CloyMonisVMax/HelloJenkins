786e1044f52ea981a0997a8f7eed84e5faa3634c
Cloy Monis <cloy.m@vmax.com>
2022-01-04 18:26:46 +0530

    first commit

---
+++ HelloJenkins/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloJenkins
+//
+//  Created by Cloy Monis on 04/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("viewDidLoad")
+    }
+
+}
+
